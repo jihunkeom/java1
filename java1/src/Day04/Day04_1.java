922626c8bfcb3ded58c7afecbba6db83dee85c62
Ji Hun Keom <samkeom@korea.ac.kr>
2021-07-19 18:17:05 +0900

    java

---
+++ java1/src/Day04/Day04_1.java
@@ -0,0 +1,71 @@
+package Day04;
+
+import java.util.Scanner;
+
+public class Day04_1 {
+	public static void main(String[] args) {
+		//과제1: 4개의 값을 입력받아 오름차순 출력 
+		Scanner input = new Scanner(System.in);
+		
+//		System.out.println("숫자 4개 입력: ");
+//		int a = input.nextInt();
+//		int b = input.nextInt();
+//		int c = input.nextInt();
+//		int d = input.nextInt();
+//		
+//		//정렬 
+//		if (a > b) {
+//			int temp = b;
+//			b = temp;
+//			a = b;
+//		}
+//		if (a > c) {
+//			int temp = c;
+//			c = a;
+//			a = temp;
+//		}
+//		if (a > d) {
+//			int temp = d;
+//			d = a;
+//			a = temp;
+//		}
+//		
+//		if (b > c) {
+//			int temp = c;
+//			c = b;
+//			b = temp;
+//		}
+//		if (b > d) {
+//			int temp = d;
+//			d = b;
+//			b = temp;
+//		}
+//		
+//		if (c > d) {
+//			int temp = d;
+//			d = c;
+//			c = temp;
+//		}
+//		
+//		System.out.println(""+a+" "+b+" "+c+" "+d);
+		
+//		System.out.println("아이디 입력: "); String id = input.next();
+//		System.out.println("비밀번호 입력: "); String password = input.next();
+//		
+//		System.out.println(id);
+//		System.out.println(password);
+//		
+//		if (id.equals("admin") && password.equals("1234")) {
+//			System.out.println("로그인 성공");
+//		} else if (!id.equals("admin")) {
+//			System.out.println("아이디 다릅니다");
+//		} else if (!password.equals("1234")) {
+//			System.out.println("비밀번호 다릅니다");
+//		}
+//
+		
+		
+		
+	}
+
+}
