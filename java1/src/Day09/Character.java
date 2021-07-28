47b0f656d39f08224f69233cc9b6b9c6c7f419a5
Ji Hun Keom <samkeom@korea.ac.kr>
2021-07-28 16:06:58 +0900

    aa

---
+++ java1/src/Day09/Character.java
@@ -0,0 +1,20 @@
+package Day09;
+
+public class Character {
+	
+	public String name;
+	protected int lv;
+	int ex;
+	private int power;
+	
+	
+	//setter, getter
+	public int getPower() {
+		return power;
+	}
+	
+	public void setPower(int newPower) {
+		this.power = newPower;
+	}
+
+}
---
+++ java1/src/Day09/Day09_1.java
@@ -0,0 +1,14 @@
+package Day09;
+
+public class Day09_1 {
+	public static void main(String[] args) {
+		Character character = new Character();
+		
+		character.name = "JiHun";
+		character.lv = 100000;
+		character.ex = 99;
+		
+		character.setPower(10);
+	}
+
+}
